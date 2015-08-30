4d64a80fd443b53c094950f16c1ff9af2016f386
Tony Han <h.bing612@gmail.com>
2015-08-30 21:49:24 +0800

    use babel loader

--- config/dev.exs
+++ config/dev.exs
@@ -20,7 +20,8 @@ config :exchat, Exchat.Endpoint,
     patterns: [
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/templates/.*(eex)$},
+      ~r{webpack\.config\.js$}
     ]
   ]
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
+var path = require('path');
+
 module.exports = {
   entry: './web/static/js/app.js',
   output: {
     path: './priv/static/js',
     filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+
+  resolve: {
+    root:[
+      path.join(__dirname, '')
+    ],
+
+    extensions: ['', '.js']
   }
+
 };
