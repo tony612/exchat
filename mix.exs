1fd2e079d7d7d2a4f3baecc20baa2c5679728935
Tony Han <h.bing612@gmail.com>
2015-10-05 00:05:46 +0800

    update phoenix

--- mix.exs
+++ mix.exs
@@ -29,11 +29,11 @@ defmodule Exchat.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 1.0.1"},
+    [{:phoenix, "~> 1.0.3"},
      {:phoenix_ecto, "~> 1.2"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.2"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:phoenix_live_reload, "~> 1.0.1", only: :dev},
      {:cowboy, "~> 1.0"}]
   end
 end
