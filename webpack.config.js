e15a0512bd85eb05d2d4085fc8e0ad2720975a72
Tony Han <h.bing612@gmail.com>
2016-02-24 23:57:48 +0800

    fix process.env.NODE_ENV setting

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ var config = {
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': {NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')}
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
