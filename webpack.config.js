e46829434ff0fd809920d627f0f07f07d4a13916
Tony Han <h.bing612@gmail.com>
2016-02-24 23:49:39 +0800

    add DefinePlugin for process.env.NODE_ENV to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,9 @@ var config = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': {NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')}
+    }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
