a4a188be478b9694d42390609703d0a2ec6ef7f6
Tony Han <h.bing612@gmail.com>
2016-04-18 23:43:15 +0800

    fix tests due to upgrading ecto to 2.0

--- config/config.exs
+++ config/config.exs
@@ -15,6 +15,8 @@ config :exchat, Exchat.Endpoint,
   pubsub: [name: Exchat.PubSub,
            adapter: Phoenix.PubSub.PG2]
 
+config :exchat, ecto_repos: [Exchat.Repo]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
