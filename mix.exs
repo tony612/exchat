d614504360a7eb992cb57320c184692aa9acdcfd
Tony Han <h.bing612@gmail.com>
2017-02-19 12:55:46 +0800

    upgrade required Elixir version

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Exchat.Mixfile do
 
   def project do
     [app: :exchat,
-     version: "0.0.1",
-     elixir: "~> 1.0",
+     version: "0.3.1",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
