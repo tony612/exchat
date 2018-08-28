25287dddff4b7ee9482422199701ff3bc8989ed1
syui <syui@syui.cf>
2018-08-29 01:15:12 +0900

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exchat.Mixfile do
   def project do
     [app: :exchat,
      version: "0.3.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.6",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
